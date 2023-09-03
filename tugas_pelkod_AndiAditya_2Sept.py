a530803166f0dc29a8b3db8b8ab2bf00e938f37b
ziihan1 <143859934+ziihan1@users.noreply.github.com>
2023-09-03 19:15:43 +0700

    Add files via upload
    tugas saya 230441100086 Andi

---
+++ tugas_pelkod_AndiAditya_2Sept.py
@@ -0,0 +1,22 @@
+panjang = int(input("masukan panjang balok = "))
+lebar = int(input("masukan lebar balok = "))
+tinggi = int(input("masukan tinggi balok = "))
+
+volume = int(panjang*lebar*tinggi)
+print("volume balok adalah =", volume)
+
+
+π = 3.14
+r_tabung = int(input("jari jari tabung "))
+t_tabung = int(input("tinggi tabung "))
+
+volume = π * r_tabung**2 * t_tabung 
+print("volume tabung adalah ", volume)
+
+
+x = 1.33
+π = 3.14
+jari_jari_bola = int(input("jari jari bola "))
+
+volume = x * π * jari_jari_bola**3
+print ("volume bola adalah ", volume)
